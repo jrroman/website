127ab1240bb14b51251fddfe9dd3f6085fe22762
John Roman <john.roman@arteric.com>
2017-10-26 13:34:52 -0400

    jrr- setting up initial boilerplate

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
