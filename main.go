6ba054017a46dfa36690d9af0306d9d8dfa8680b
John Roman <john.roman@arteric.com>
2017-10-26 22:01:03 -0400

    jrr- playing with forms

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 )
 
 type pageData struct {
-	Title string
+	Title     string
+	FirstName string
+	LastName  string
 }
 
 var tmpl *template.Template
@@ -17,13 +19,33 @@ func init() {
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
 	http.HandleFunc("/", index)
+	http.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	pd := pageData{
 		Title: "Index",
+	}
+
+	err := tmpl.ExecuteTemplate(w, "index.gohtml", pd)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	pd := pageData{
+		Title: "Thank You!",
+	}
+
+	if r.Method == http.MethodPost {
+		pd.FirstName = r.FormValue("fname")
+		pd.LastName = r.FormValue("lname")
 	}
 
 	err := tmpl.ExecuteTemplate(w, "index.gohtml", pd)
